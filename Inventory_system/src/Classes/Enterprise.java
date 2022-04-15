b9658922c40d77b2c40d23670358647071db45da
Sherathia <sherathia.a@northeastern.edu>
2022-04-15 15:18:01 -0400

    Added Classes and New screen for fundraisers.
    Added Classes and New screen for fundraisers.

---
+++ Inventory_system/src/Classes/Enterprise.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author aesha
+ */
+public class Enterprise extends Network{
+    
+    private String enterpriseName;
+    public Enterprise()
+    {
+                
+    }
+}
