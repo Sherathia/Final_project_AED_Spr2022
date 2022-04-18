60136572f544b2addf2dc0a7f34baf44ad9c6fdd
Sherathia <sherathia.a@northeastern.edu>
2022-04-18 12:44:13 -0400

    Modified hierarchy structure
    Added flow like Ecosystem-> NetworkDirectory -> Network -> enterpriseDir -> Enterprise-> OrgDirectory -> Org -> All Dir of Orgs in org

--- Inventory_system/src/Users/UserAccount.java
+++ Inventory_system/src/Users/UserAccount.java
@@ -14,4 +14,44 @@ public class UserAccount {
     //private Role role;
     private String name;
     private String network;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNetwork() {
+        return network;
+    }
+
+    public void setNetwork(String network) {
+        this.network = network;
+    }
+    
+     public UserAccount(String name,String username, String password, 
+             String network) {
+         this.name = name;
+         this.username = username;
+         this.password = password;
+         this.network = network;
+    }
 }
