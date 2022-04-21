056fbd8f447a6a4f63f84d0290040482b5996c43
Sherathia <sherathia.a@northeastern.edu>
2022-04-21 16:09:46 -0400

    Added logic for Work requests screen for Admin login
    Added logic for Work requests screen for Admin login
    1) approve request
    2) reject request
    3) hold request

--- Inventory_system/Create_tables.sql
+++ Inventory_system/Create_tables.sql
@@ -18,6 +18,8 @@ Create_datetime varchar(200));
 
 select * from FinalProj_Organization;
 
+select distinct Enterrpise from FinalProj_Organization where Country ="US";
+
 create table FinalProj_Users
 (Name varchar(200),
 UserName varchar(200),
@@ -28,15 +30,47 @@ Password varchar(200),
 Organization varchar(200),
 Enterprise varchar(200),
 Network varchar(200)
-)
+);
+
+delete from FinalProj_Users where username <>"Admin";
+
+
 
 create table FinalProj_Organization
 (
 OrgName varchar(200),
 OrgType varchar(200));
 
-select * from FinalProj_Organization;
+select * from FinalProj_Users;
+
+update FinalProj_Users
+set Organization="Admin"
+where name ="Admin";
 
 insert into FinalProj_Organization values("ABC","Clothing","US","Warehouse","Boston","12-Dec-22");
 
-insert into finalproj_enterprise values ("
+
+create table FinalProj_AdminRequests
+(
+RequestID varchar(200),
+Name varchar(200),
+Network varchar(200),
+Enterprise varchar(200),
+Organization varchar(200),
+quantity varchar(200),
+Description varchar(200),
+Location varchar(200),
+RequestedDate varchar(200),
+ApprovalDate varchar(200),
+status varchar(200),
+Comments varchar(200),
+Attrib1 varchar(200),
+Attrib2 varchar(200))
+
+
+
+
+
+
+
+
