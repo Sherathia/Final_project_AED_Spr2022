f4a395d8b1c6a9d289085acaa5b9e9698bc72026
Sherathia <sherathia.a@northeastern.edu>
2022-04-25 13:09:18 -0400

    added changes in registration screen to merge email code changes
    added changes in registration screen to merge email code changes

--- Inventory_system/src/UI/RegistrationScreen.java
+++ Inventory_system/src/UI/RegistrationScreen.java
@@ -4,11 +4,19 @@
  */
 package UI;
 
+import Business.SendMail;
 import java.awt.CardLayout;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -365,6 +373,7 @@ public class RegistrationScreen extends javax.swing.JPanel {
             return;
         }
         
+        sendMail("Registration","Registered Successfully",EmailId);
         
         try{
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -438,6 +447,40 @@ public class RegistrationScreen extends javax.swing.JPanel {
        
     }//GEN-LAST:event_pwd1FocusLost
 
+    public void sendMail(String subject, String text, String to) {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
+        Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("inventorymanagementaas@gmail.com", "aas051997");
+            }
+        });
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("inventorymanagementaas@gmail.com"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress());
+
+//            if (id != null && customer != null && ticket != null) {
+            message.setSubject(subject);
+            message.setText("Dear ," + "" + ".\n\nRegards,\nInventory Management Admin");
+//            } 
+//        else {
+//            message.setSubject("Thanks for registering with us!");
+//            message.setText("You have successfully registered yourself!");
+//            }
+            Transport.send(message);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+    
     private void pwd2FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_pwd2FocusLost
         // TODO add your handling code here:
          char pass1[]= pwd1.getPassword();
