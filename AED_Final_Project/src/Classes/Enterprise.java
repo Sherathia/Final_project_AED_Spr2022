b7064e29f64fd14ac132290c0db820bda0c4cf72
sai srujan <penta.s@northeastern.edu>
2022-04-25 21:44:25 -0400

    added few classes

--- AED_Final_Project/src/Classes/Enterprise.java
+++ AED_Final_Project/src/Classes/Enterprise.java
@@ -4,10 +4,70 @@
  */
 package Classes;
 
+import Enterprise.Government.GovernmentDirectory;
+import Enterprise.Medical.BloodBankDirectory;
+import Enterprise.Medical.HospitalDirectory;
+import Enterprise.Medical.InsuranceDirectory;
+import Enterprise.Medical.MedicalEquipManuDirectory;
+import Enterprise.Medical.VaccineManuDirectory;
+import Enterprise.Voluntary.FundraiserDirectory;
+import Enterprise.Voluntary.NGODirectory;
+import Enterprise.Warehouse.ClothingChainDirectory;
+import Enterprise.Warehouse.FoodMarketRtlDirectory;
+import Enterprise.Warehouse.TransportAgenciesDirectory;
+
 /**
  *
  * @author saisr
  */
-public class Enterprise {
+public abstract class Enterprise extends Organization {
+    
+    private EnterpriseType enterpriseType;
+    public Enterprise(String name, EnterpriseType type) {
+        super(name);
+        this.enterpriseType=type;
+        organizationDirectory=new OrganizationDirectory();
+    }
+    
+    
+        public enum EnterpriseType{
+            Medical("Medical"),
+            Voluntary("Voluntary"),
+            Government("Government"),
+            Warehouse("Warehouse");
+            
+            private String type;
+            
+            private EnterpriseType(String type){
+                this.type = type;
+            }
+            public String getValue() {
+                return type;
+            }
+            @Override
+            public String toString(){
+                return type;
+            }
+        }
+
+    public EnterpriseType getEnterpriseType() {
+        return enterpriseType;
+    }
+
+    public void setEnterpriseType(EnterpriseType enterpriseType) {
+        this.enterpriseType = enterpriseType;
+    }
+        
+    private OrganizationDirectory organizationDirectory;
+
+    public OrganizationDirectory getOrganizationDirectory() {
+        return organizationDirectory;
+    }
+
+    public void setOrganizationDirectory(OrganizationDirectory organizationDirectory) {
+        this.organizationDirectory = organizationDirectory;
+    }
+    
+    
     
 }
