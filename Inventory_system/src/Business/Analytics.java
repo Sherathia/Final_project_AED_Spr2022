d75583507964b9584dbb0e085fa79d61eda6d7e9
Anvitha Lakshmisha <98431766+anvithalakshmisha@users.noreply.github.com>
2022-04-28 23:41:01 -0400

    add Jfree chart code

--- Inventory_system/src/Business/Analytics.java
+++ Inventory_system/src/Business/Analytics.java
@@ -4,10 +4,29 @@
  */
 package Business;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot3D;
+import org.jfree.data.general.DefaultPieDataset;
+
 /**
  *
  * @author anvithalakshmisha
  */
 public class Analytics {
-    
+
+    public void drawChart(int acceptedRequest, int rejectedRequest, int totalRequest) {
+        DefaultPieDataset pieDataSet = new DefaultPieDataset();
+        pieDataSet.setValue("Accepted", Integer.valueOf(acceptedRequest));
+        pieDataSet.setValue("Rejected", Integer.valueOf(rejectedRequest));
+        pieDataSet.setValue("Total request", Integer.valueOf(totalRequest));
+
+        JFreeChart chart = ChartFactory.createPieChart3D("request analytics", pieDataSet, true, true, true);
+        PiePlot3D p = (PiePlot3D) chart.getPlot();
+        ChartFrame frame = new ChartFrame("Pie chart", chart);
+        frame.setVisible(true);
+        frame.setSize(450, 500);
+
+    }
 }
